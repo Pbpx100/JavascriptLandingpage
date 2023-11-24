858d9e50598421e2cdf6c2815ee65c11cc3bb95b
Paul B. P <38941153+Pbpx100@users.noreply.github.com>
2023-11-24 17:30:25 +0100

    Update modal.js

--- modal.js
+++ modal.js
@@ -6,17 +6,6 @@ const closeBtn = document.querySelectorAll(".close")
 const closeBtnModal = document.getElementById("button-close")
 const x = document.getElementById("myTopnav");
 
-//Add or remove class responsive to get style
-function editNav() {
-  if (x.classList.contains("topnav") || x.classList.contains("movil")) {
-    if (!x.classList.contains("responsive")) {
-      x.className += " responsive";
-    } else {
-      x.classList.remove("responsive");
-
-    }
-  }
-}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
